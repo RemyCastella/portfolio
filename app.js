0e3c4e4879082ea13a15804c504c5fab588bece0
Remy Castella <remy.jpen@gmail.com>
2025-03-20 20:01:06 +0900

    implement project links togglable on click

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 const logosContainer = document.querySelector('.logos-slide');
+const projectImage = document.querySelectorAll('.project-image');
+const projectLinks = document.querySelectorAll('.project-links');
+
+projectImage.forEach((image, index) => {
+  image.addEventListener('click', () => {
+    projectLinks[index].classList.toggle('active');
+  })
+});
+
+
 document.querySelectorAll('.logo').forEach(logo => {
   const clone = logo.cloneNode(true);
   logosContainer.appendChild(clone);

25cc4d3d772441717a722a7720e0c2dbe91b8a36
Remy Castella <remy.jpen@gmail.com>
2025-03-31 10:40:53 +0900

    remove JavaScript for logos slider

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import scroll from './utils/scroll.js';
 import projectLinks from './utils/projectLinks.js';
 
-const logosContainer = document.querySelector('.logos-slide');
-document.querySelectorAll('.logo').forEach(logo => {
-  const clone = logo.cloneNode(true);
-  logosContainer.appendChild(clone);
-})
+// const logosContainer = document.querySelector('.logos-slide');
+// document.querySelectorAll('.logo').forEach(logo => {
+//   const clone = logo.cloneNode(true);
+//   logosContainer.appendChild(clone);
+// })
 
 projectLinks();
 document.addEventListener('scroll', scroll, { passive: true });
